94284352f6b9c03018703170010b9729d544718d
Florian Liehr <florian.liehr@dataciders.com>
2025-07-27 21:20:42 +0200

    Init

---
+++ src/main/java/de/megathon/server/MegathonApplication.java
@@ -0,0 +1,13 @@
+package de.megathon.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/megathon/server/MegathonApplicationTests.java
@@ -0,0 +1,14 @@
+package de.megathon.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //TODO @fliehr (27.07.2025) Fill with actual tests
+    }
+
+}
